f65712bd980a663e2f60b140d0a43f3332197e02
McCauley Peeler <mccauleypeeler@gmail.com>
2018-09-28 17:30:37 -0400

    new error 'token mismatch' 'invalid app credential. added delegate methods for phone auth

--- WizardUser/AppDelegate.swift
+++ WizardUser/AppDelegate.swift
@@ -18,7 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, FUIAuthDelegate {
     
     override init() {
         FirebaseApp.configure()
-
+        
     }
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
@@ -49,21 +49,33 @@ class AppDelegate: UIResponder, UIApplicationDelegate, FUIAuthDelegate {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
     
-//    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
-//        Auth.auth().setAPNSToken(deviceToken, type: AuthAPNSTokenTypeProd)
-//
-//    }
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        Auth.auth().setAPNSToken(deviceToken, type: AuthAPNSTokenType.prod)
+
+    }
     
-//    func application(_ application: UIApplication,
-//                     didReceiveRemoteNotification notification: [AnyHashable : Any],
-//                     fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
-//        if Auth.auth().canHandleNotification(notification) {
-//            completionHandler(.noData)
-//            return
+    func application(_ application: UIApplication,
+                     didReceiveRemoteNotification notification: [AnyHashable : Any],
+                     fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
+        if Auth.auth().canHandleNotification(notification) {
+            completionHandler(UIBackgroundFetchResult.noData)
+            return
+        }
+        // This notification is not auth related, developer should handle it.
+//        handleNotification(notification)
+    }
+    
+    
+//    func application(_ application: UIApplication, open url: URL,
+//                     options: [UIApplication.OpenURLOptionsKey : Any]) -> Bool {
+//        if Auth.auth().canHandle(url) {
+//            return true
 //        }
-//        // This notification is not auth related, developer should handle it.
-////        handleNotification(notification)
+//        // URL not auth related, developer should handle it.
 //    }
+
+    
+    
     
     
 
